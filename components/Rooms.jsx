3fa6f88149ebed8e9f2a2dbbd4f5f957f9e70097
SimonAnastasov <simonanastasov2001@gmail.com>
2022-03-23 00:15:37 +0100

    Removed Express logic

--- components/Rooms.jsx
+++ components/Rooms.jsx
@@ -9,15 +9,6 @@ import { Grid } from '@mui/material';
 
 import axios from 'axios';
 
-// export async function getServerSideProps(context) {
-
-//   return {
-//     props: {
-//       data
-//     }, // will be passed to the page component as props
-//   }
-// }
-
 const Rooms = () => {
   let roomsInfo = useSelector(state => state.rooms);
   let rooms = roomsInfo.rooms;
@@ -27,24 +18,7 @@ const Rooms = () => {
     rooms = rooms.filter(room => room.name.toLowerCase().includes(filter.toLowerCase()));
   }
 
-  function increment() {
-    axios.get('/api/counter')
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
-
-  if (!rooms.length) {
-    return <>
-      <p style={{color: 'white'}}>Counter: 0</p>
-      <button onClick={() => increment()}>Increment Counter</button>
-    </>
-  }
-
-  // if (!rooms.length) return <p style={{color: 'white'}}>No rooms created</p>;
+  if (!rooms.length) return <p style={{color: 'white'}}>No rooms created</p>;
 
   return (
     <Grid container columnSpacing={12} justifyContent="center" alignItems="center">
--- pages/index.js
+++ pages/index.js
@@ -7,29 +7,19 @@ import Head from 'next/head';
 import { CssBaseline } from '@mui/material';
 
 export default function Home() {
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+
 
   return (
     <div>
       <Head>
         <title>Space Uno (Uno 3.0)</title>
-        {/* <meta name="google-signin-client_id" content="273257248097-vvc87cj68g7dqdhdmkr27a8b1hhnp3nh.apps.googleusercontent.com"></meta>
-        <script src="https://apis.google.com/js/platform.js" async defer></script> */}
       </Head>
       <CssBaseline/>
       <Header/>
       <QuickNav/>
       <Rooms/>
 
-      {/* <div className="g-signin2" data-onsuccess="onSignIn"></div> */}
-
-      {/* Continue with NEXTAUTH.js */}
+      
     </div>
   )
 }
